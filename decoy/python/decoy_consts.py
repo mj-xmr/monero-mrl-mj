93882ae5c7314b0d07a9e4c2c3d336dfa69ff496
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-05-13 17:35:18 +0200

    Copied the relevant constants

--- decoy/python/decoy_consts.py
+++ decoy/python/decoy_consts.py
@@ -18,9 +18,16 @@ def get_data_path(file_name):
     if os.path.isfile(optimistic):
         return optimistic
     return DIR_TMP_ALT + file_name
-    
+
 PATH_MUL_2_RATIO_GOOD = get_data_path('mrl_mul_2_ratio_good.csv')
 PATH_GAMMA_PDF = get_data_path('mrl_gamma_distrib.csv')
+CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE =             10
+
+DIFFICULTY_TARGET_V2  =                          120  # // seconds
+DIFFICULTY_TARGET_V1  =                          60   # // seconds - before first fork
+DEFAULT_UNLOCK_TIME = (CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE * DIFFICULTY_TARGET_V2)
+RECENT_SPEND_WINDOW = (15 * DIFFICULTY_TARGET_V2)
+
 
 def load_data(path):
     print("Loading:", path)
