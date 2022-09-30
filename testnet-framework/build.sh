4dfbfeacdfa9f9223c237843a1cd5e297ab665ce
mj-xmr <63722585+mj-xmr@users.noreply.github.com>
2022-09-30 18:32:58 +0200

    Build script

---
+++ testnet-framework/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+DIR_THIS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+DIR=/tmp/monero
+PROJ=monero-mj
+mkdir -p $DIR && cd $DIR
+
+if [ ! -d $PROJ ]; then
+	git clone https://github.com/mj-xmr/$PROJ.git 
+	cd $PROJ
+	git checkout mrl-fingerprint-fee
+else
+	cd $PROJ
+fi
+
+
+
+
+
+
+cp -v $DIR_THIS/Dockerfile .
+
+sudo docker build .
